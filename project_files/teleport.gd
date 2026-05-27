f9bb878325ca9c97cdc376d0221e2b3824e01732
BrtKST <b.kostuj1011@gmail.com>
2026-05-27 21:34:24 +0200

    Poprawa obrotu

--- project_files/teleport.gd
+++ project_files/teleport.gd
@@ -2,6 +2,12 @@ extends XRController3D
 
 @onready var ray: RayCast3D = $TeleportRay
 @onready var marker: MeshInstance3D = $TeleportMarker
+
+#Zmienne do obrotu - kąt, flaga 
+@export var snap_turn_angle: float = 45.0
+@export var turn_deadzone: float = 0.5
+var can_snap_turn: bool = true
+
 var xr_origin: XROrigin3D
 var xr_camera: XRCamera3D
 
@@ -17,6 +23,38 @@ func _process(_delta: float) -> void:
 		marker.visible = true
 	else:
 		marker.visible = false
+		
+	# Funkcja obrotu
+	handle_snap_turn()
+
+# OBRÓT SKOKOWY
+func handle_snap_turn() -> void:
+	var joy: Vector2 = get_vector2("thumbstick")
+
+	# odblokuj obrót jeżeli puści gałkę
+	if abs(joy.x) < turn_deadzone:
+		can_snap_turn = true
+		return
+
+	# Jeśli gracz wychylił gałkę i obrót jest dozwolony
+	if can_snap_turn:
+		# Zapisujemy pozycję głowy przed obrotem
+		var pos_before = xr_camera.global_position
+
+		# Wykonujemy obrót bazy
+		if joy.x > 0:
+			xr_origin.rotate_y(deg_to_rad(-snap_turn_angle)) # W prawo
+		else:
+			xr_origin.rotate_y(deg_to_rad(snap_turn_angle))  # W lewo
+
+		# Korygujemy przesunięcie (żeby gracz nie kręcił się jak na karuzeli, 
+		# tylko obracał w miejscu, w którym aktualnie stoi jego głowa)
+		var pos_after = xr_camera.global_position
+		var delta_pos = pos_before - pos_after
+		xr_origin.global_position += delta_pos
+		
+		# Blokujemy obrót, dopóki gracz nie puści gałki
+		can_snap_turn = false
 
 func teleport_now() -> void:
 	if not ray.is_colliding():
@@ -30,9 +68,8 @@ func teleport_now() -> void:
 	origin_tf.origin = Vector3(target.x - cam_offset.x, target.y, target.z - cam_offset.z)
 	xr_origin.global_transform = origin_tf
 
-
 func _on_button_pressed(button_name: String) -> void:
-	print("Wciśnięto przycisk: ", button_name) # Dodaj to
+	print("Wciśnięto przycisk: ", button_name) 
 	if button_name == "trigger_click":
-		print("RayCast uderza w: ", ray.is_colliding()) # Dodaj to
+		print("RayCast uderza w: ", ray.is_colliding()) 
 		teleport_now()
